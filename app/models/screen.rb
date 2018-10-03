0331e2f17dff10ac5116b00457413be7ac735c0d
Tech-A65 <tech-a65@Tech-A56s-MacBook-Air.local>
2018-10-03 08:54:42 +0100

    Validation Edits

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -1,4 +1,6 @@
 class Screen < ApplicationRecord
   belongs_to :cinema
   validates :film_playing, presence: true, format: {:with => /[a-zA-Z\-\_20%]+/}
+  validates :cinema_id, uniqueness: true, presence: true
+  validates :screen_no, uniqueness: true, presence: true
 end
